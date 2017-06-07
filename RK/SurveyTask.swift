172350233836ffc921c0e0170095187855838072
Yaseen Majeed <yaseenmajeed5@hotmail.com>
2017-06-07 05:35:14 +0530

    added research kit framework and dependecies

---
+++ RK/SurveyTask.swift
@@ -0,0 +1,9 @@
+//
+//  SurveyTask.swift
+//  RK
+//
+//  Created by Yaseen on 05/06/17.
+//  Copyright © 2017 Yaseen. All rights reserved.
+//
+
+import Foundation
---
+++ RK/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  RK
+//
+//  Created by Yaseen on 03/06/17.
+//  Copyright © 2017 Yaseen. All rights reserved.
+//
+
+import UIKit
+import ResearchKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func consentTapped(sender : AnyObject) {
+        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func surveyTapped(sender : AnyObject) {
+        let taskViewController = ORKTaskViewController(task: SurveyTask, taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController : ORKTaskViewControllerDelegate {
+    
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        //Handle results with taskViewController.result
+        taskViewController.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
